0243d69a31731ad6dd0135097f4d8e45c3a6869e
Gonzalo <gonzalo.hernandez.1293@gmail.com>
2019-02-03 12:45:08 +0100

    updated data

--- download_plane_data.py
+++ download_plane_data.py
@@ -15,15 +15,26 @@ def parse_plane(plane_url, plane_name):
     soup = BeautifulSoup(plane_html, "html.parser")
 
     plane_rows = soup.find_all("tr", {"class": "trtab"})
-    cells_titles = ["msn", "type", "airline", "first_flight", "registration", "status"]
+    # Sometimes the tables will have more columns than expected so we have to
+    # check that we download the ones we want
+    cells_titles = ["msn", "type", "airline", "first flight", "registration", "status"]
+
+    table_header_row = soup.find("tr", {"class": "textenu"}).find_all("td")
+    parsed_cell_titles = []
+    for header_col in table_header_row:
+        parsed_cell_titles.append(header_col.text.strip())
+
     planes_matrix = []
     for row in plane_rows:
         cells = row.find_all("td")
         plane_row = []
         plane_row.append(plane_name)
-        for cell_and_title in zip(cells, cells_titles):
+        for cell_and_title in zip(cells, parsed_cell_titles):
+            title_currently_parsing = cell_and_title[1].lower()
+            # If this is an extra column that we don't want, skip it
+            if not title_currently_parsing in cells_titles:
+                continue
             text = cell_and_title[0].text.strip()
-            title = cell_and_title[1]
             plane_row.append(text)
         planes_matrix.append(plane_row)
     return planes_matrix
